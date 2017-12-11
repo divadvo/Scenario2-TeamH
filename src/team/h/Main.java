80d2445f975f91375ca98bccf3f1122270c2715a
Nequo <nedxtreme@gmail.com>
2017-12-11 14:28:30 +0000

    Removed debugging for parser

--- src/team/h/Main.java
+++ src/team/h/Main.java
@@ -3,6 +3,6 @@ package team.h;
 public class Main {
 
     public static void main(String[] args) {
-        new ProblemParser("input/simple.rfp").parse();
+        new ProblemParser("input/problems.rfp").parse();
     }
 }
