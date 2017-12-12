5b69b48a454670918cb19f9b9a6c569325cc0792
Nequo <nedxtreme@gmail.com>
2017-12-12 11:05:29 +0000

    Added a Test to check if angles are calculated correctly using a simple square

--- src/test/PolygonTest.java
+++ src/test/PolygonTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.junit.Test;
+import team.h.PartsOfPolygon;
 import team.h.Point;
 import team.h.Polygon;
 
@@ -13,10 +14,26 @@ import static org.junit.Assert.assertThat;
 public class PolygonTest
 {
     @Test
-    public void calculateThatAreaCalculatedCorrectly()
+    public void areaIsCalculatedCorrectly()
     {
         Polygon polygon = new Polygon(getSamplePoints());
         assertThat(polygon.calculateArea(), is(12.0));
+    }
+
+    @Test
+    public void angleIsCalculatedCorrectly(){
+        Point point1 = new Point(0,0);
+        Point point2 = new Point(1,0);
+        Point point3 = new Point(1,1);
+        Point point4 = new Point(0,1);
+        List<Point> points = new ArrayList<>();
+        points.add(point1);
+        points.add(point2);
+        points.add(point3);
+        points.add(point4);
+        Polygon test = new Polygon(points);
+        List<PartsOfPolygon.Angle> angles = test.getAngles();
+        assertThat(angles.get(0).getValue(), is(Math.PI/2));
     }
 
     private List<Point> getSamplePoints()
